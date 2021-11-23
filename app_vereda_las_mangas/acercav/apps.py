525c80e29558e6a45549d1723557ccdac44de82b
Diaz Gallego <jjdiazg@indracompany.com>
2021-11-23 15:46:05 -0500

    Commit correciones casi final

--- app_vereda_las_mangas/acercav/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AcercavConfig(AppConfig):
-    name = 'acercav'
--- app_vereda_las_mangas/acercav/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Acerca(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField(max_length=500)
